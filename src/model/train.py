34952fa33314cddd127898e289ff76026c443461
Joao Pedro Martins <joaopedro.martins@microsoft.com>
2022-07-27 19:41:50 +0200

    I hate linting

--- src/model/train.py
+++ src/model/train.py
@@ -21,7 +21,7 @@ def main(args):
     X_train, X_test, y_train, y_test = process_data(df)
 
     # train model
-    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)  # noqa: F841, F501
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)  # noqa
 
 
 def get_csvs_df(path):
